d5f9f319d86fc4d69fea43a4fb093f2e5b679f01
ZXu <88455604+tgamZheng@users.noreply.github.com>
2024-05-23 15:35:27 -0400

    persist api key in session state

--- Chatbot.py
+++ Chatbot.py
@@ -2,10 +2,17 @@ from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    if 'key' not in st.session_state:
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        st.session_state['key'] = openai_api_key
+    else:
+        openai_api_key = session_state['key']
+        
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+
 
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
